474e9edf6d9ad0f9f76146f8efe403a1df5ee980
evaclemente <evaclemente@users.noreply.github.com>
2019-07-14 18:42:06 +0200

    Form Completo

--- src/app/formpersona/formpersona.page.ts
+++ src/app/formpersona/formpersona.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Persona } from '../Persona';
+import { from } from 'rxjs';
+import { DbServiceService } from '../db-service.service';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-formpersona',
@@ -7,9 +13,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormpersonaPage implements OnInit {
 
-  constructor() { }
+ nombre: string;
+ pass: string;
+ rol: any;
+
+  constructor(private router: Router,
+              private http: HttpClient,
+              private dbService: DbServiceService ) { }
 
   ngOnInit() {
+  }
+
+  Pon() {
+    console.log('Estoy añadiendo a: ' + this.nombre + this.pass + this.rol);
+    this.dbService.PonPersona(new Persona(this.nombre,
+                                          this.pass,
+                                          this.rol,
+                                          false,
+                                          false,
+                                          false,
+                                          false));
+  }
+
+  VolverALista() {
+    this.router.navigate(['/lista']);
   }
 
 }
