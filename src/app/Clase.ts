db41c31d6005d9f8ffe9a8982d6752d38bdc1473
evaclemente <evaclemente@users.noreply.github.com>
2019-07-26 12:17:38 +0200

    Clase component

---
+++ src/app/Clase.ts
@@ -0,0 +1,12 @@
+export class Clase {
+
+    id: string;
+    admin: string;
+    avatares: boolean;
+
+    constructor(id: string, admin: string, avatares: boolean) {
+      this.id = id;
+      this.admin = admin;
+      this.avatares = avatares;
+    }
+}
